2506258fcf2f9039405fabb70c4501b4636bc3c5
Nikita Medvedev <some.redteapot@gmail.com>
2018-07-12 17:35:59 +0700

    Move template system to Jinja2 backend

--- bbya/settings.py
+++ bbya/settings.py
@@ -55,6 +55,15 @@ ROOT_URLCONF = 'bbya.urls'
 
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [os.path.join(BASE_DIR, 'templates/jinja2')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'environment': 'bbya.jinja2.environment',
+            'extensions': ['pipeline.jinja2.PipelineExtension'],
+        }, 
+    },
+    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
