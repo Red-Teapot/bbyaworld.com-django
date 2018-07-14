e2fa490651e5d5ee2cc68dee7af1b4d7863352f4
Nikita Medvedev <some.redteapot@gmail.com>
2018-07-14 14:59:55 +0700

    Make server status checker JS work with new server-status endpoint

--- website/static/website/js/server-status.js
+++ website/static/website/js/server-status.js
@@ -1,23 +1,25 @@
 $(function() {
-    $.get('/game-server-status', null, function(data) {
-        $('#server-status-indicator').addClass(data['status'] ? 'on' : 'off');
+    $.get('/server-status', null, function(data) {
+        $('#server-status-indicator').addClass(data['online'] ? 'on' : 'off');
 
-        $('#server-players > *').remove();
-        data['players'].forEach(function(element, index, array) {
-            var item = $(document.createElement('li'));
-            item.addClass('item');
+        if(data['online']) {
+            $('#server-players > *').remove();
+            data['players']['list'].forEach(function(element, index, array) {
+                var item = $(document.createElement('li'));
+                item.addClass('item');
 
-            var head = $(document.createElement('img'));
-            head.attr('src', 'https://minotar.net/helm/' + element + '/32');
+                var head = $(document.createElement('img'));
+                head.attr('src', 'https://minotar.net/helm/' + element + '/32');
 
-            var nickname = $(document.createTextNode(element));
+                var nickname = $(document.createTextNode(element));
 
-            item.append(head);
-            item.append(nickname);
+                item.append(head);
+                item.append(nickname);
 
-            $('#server-players').append(item);
-        });
+                $('#server-players').append(item);
+            });
 
-        $('#server-players-count').text(data['players'].length);
+            $('#server-players-count').text(data['players']['online']);
+        }
     });
 });
