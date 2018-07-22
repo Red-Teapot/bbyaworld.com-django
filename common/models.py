f64051467a1335190054f8efd736737b5407a3f7
Nikita Medvedev <some.redteapot@gmail.com>
2018-07-22 17:16:01 +0700

    Change MiscStorageEntry value field type to TextField

--- common/models.py
+++ common/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class MiscStorageEntry(models.Model):
     key = models.CharField(max_length=64, db_index=True, primary_key=True)
-    value = models.CharField(max_length=256)
+    value = models.TextField()
