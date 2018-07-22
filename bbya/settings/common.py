743f701cba630e35cbf1c258a7ddb68600bfb97e
Nikita Medvedev <some.redteapot@gmail.com>
2018-07-22 17:53:10 +0700

    Add Pipeline HTML minifier middleware

--- bbya/settings/common.py
+++ bbya/settings/common.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pipeline.middleware.MinifyHTMLMiddleware',
 ]
 
 ROOT_URLCONF = 'bbya.urls'
