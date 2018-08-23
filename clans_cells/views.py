ec75d9b798946c86b4c610779334f5574bbeb2e4
Nikita Medvedev <some.redteapot@gmail.com>
2018-08-23 21:02:24 +0700

    Fix last_update date loading
    Move it from static class fields to functions

--- clans_cells/views.py
+++ clans_cells/views.py
@@ -10,9 +10,13 @@ from .models import ClanModel
 class IndexView(ListView):
     model = ClanModel
     template_name = 'clans_cells/index.html'
-    extra_context = {
-        'last_update': datetime.datetime.fromtimestamp(int(MiscStorage.get('clans_cells.last_update', '0'))),
-    }
+
+    def get_context_data(self):
+        extra_context = dict()
+
+        extra_context['last_update'] = datetime.datetime.fromtimestamp(int(MiscStorage.get('clans_cells.last_update', '0')))
+
+        return super().get_context_data(**extra_context)
 
     def get_queryset(self):
         return self.model.objects.order_by('order')  # pylint: disable=no-member
--- player_online_stats/views.py
+++ player_online_stats/views.py
@@ -1,4 +1,4 @@
-import math, datetime
+import math, datetime, random
 
 from django.shortcuts import render
 from django.views.generic.list import ListView
@@ -11,9 +11,13 @@ class StatsView(ListView):
     model = StatsEntry
     paginate_by = 50
     template_name = 'player_online_stats/index.html'
-    extra_context = {
-        'last_update': datetime.datetime.fromtimestamp(int(MiscStorage.get('player_online_stats.last_update', '0'))),
-    }
+
+    def get_context_data(self):
+        extra_context = dict()
+
+        extra_context['last_update'] = datetime.datetime.fromtimestamp(int(MiscStorage.get('player_online_stats.last_update', '0')))
+
+        return super().get_context_data(**extra_context)
 
     def get_queryset(self):
         return self.model.objects.filter(time__gte=60).order_by('-time')  # pylint: disable=no-member
