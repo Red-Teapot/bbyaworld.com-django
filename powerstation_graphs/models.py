68144eed6d0ec2b08ee49d24258b4ca8d2f36ef8
Nikita Medvedev <some.redteapot@gmail.com>
2018-08-24 19:21:49 +0700

    Create Measurement model for powerstation_graphs app

--- powerstation_graphs/models.py
+++ powerstation_graphs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Measurement(models.Model):
+    type = models.SmallIntegerField(default=-1, db_index=True)
+    value = models.BigIntegerField(default=-1000)
+    datetime = models.DateTimeField(auto_now=True, db_index=True)
+
+    value_types = {
+        'wheat_pp': 1,
+        'coal_pp': 2,
+        'diamond_pp': 3,
+        'exp_market': 4,
+    }
