1f6e77e3cd6fbbbfd674b5b0d85b1d5a5382265a
Nikita Medvedev <some.redteapot@gmail.com>
2018-08-25 23:48:06 +0700

    Split plot into multiple plots (separate ones for powerstations and experience market)

--- powerstation_graphs/static/powerstation_graphs/js/graphs.js
+++ powerstation_graphs/static/powerstation_graphs/js/graphs.js
@@ -1,14 +1,14 @@
 $(function() {
-    const plot_elem = document.getElementById('plot');
-
-    const trace_names = {
+    var trace_names = {
         wheat_pp: 'Пшеница',
         coal_pp: 'Уголь',
         diamond_pp: 'Алмазы',
         exp_market: 'Опыт',
+        buy: 'Покупка',
+        sell: 'Продажа',
     };
 
-    const layout = {
+    var layout = {
         xaxis: {
             title: 'Время'
         },
@@ -17,27 +17,40 @@ $(function() {
         }
     };
 
-    var traces = [];
-    var trace_indices_by_name = {};
+    function formatDate(date) {
+        var result = '';
 
-    $.get('get-data', null, function(data) {
+        var day = date.getDate();
+        result += day < 10 ? '0' + day : day;
+        result += '.';
+
+        var month = date.getMonth() + 1;
+        result += month < 10 ? '0' + month : month;
+        result += '.';
+
+        var year = date.getFullYear();
+        result += year;
+
+        return result;
+    }
+
+    function createPlot(plotType, dataTypes, data, title) {
+        var traces = [];
+        var traceIndicesByName = [];
+
+        var plotDivId = 'plot_' + plotType;
+
+        $('#plot').append($('<div id="' + plotDivId + '"></div>'));
+        var plotDiv = document.getElementById(plotDivId);
+        var checkboxForm = $(plotDiv).append('<form></form>');
+
+
         $.each(data, function(type, values) {
+            if(!dataTypes.includes(type))
+                return true;
+
             var dates = values.dates.map((val) => {
-                var date = new Date(val);
-                var result = '';
-
-                var day = date.getDate();
-                result += day < 10 ? '0' + day : day;
-                result += '.';
-
-                var month = date.getMonth() + 1;
-                result += month < 10 ? '0' + month : month;
-                result += '.';
-
-                var year = date.getFullYear();
-                result += year;
-
-                return result;
+                return formatDate(new Date(val));
             });
 
             var trace = {
@@ -47,28 +60,86 @@ $(function() {
                 name: trace_names[type]
             };
 
-            trace_indices_by_name[type] = traces.length;
+            traceIndicesByName[type] = traces.length;
             traces.push(trace);
         });
 
-        Plotly.plot(plot_elem, traces, layout);
+        layout.title = title;
+        Plotly.plot(plotDiv, traces, layout);
 
-        Plotly.restyle(plot_elem, 'visible', $('#wheat_pp_checkbox')[0].checked, trace_indices_by_name['wheat_pp']);
-        Plotly.restyle(plot_elem, 'visible', $('#coal_pp_checkbox')[0].checked, trace_indices_by_name['coal_pp']);
-        Plotly.restyle(plot_elem, 'visible', $('#diamond_pp_checkbox')[0].checked, trace_indices_by_name['diamond_pp']);
-        Plotly.restyle(plot_elem, 'visible', $('#exp_market_checkbox')[0].checked, trace_indices_by_name['exp_market']);
-    });
+        if(dataTypes.length > 1) {
+            $.map(dataTypes, (k) => {
+                var id = k;
+                var label_text = k in trace_names ? trace_names[k] : k;
+                
+                var paragraph = $('<p></p>');
+                var checkbox = $('<input type="checkbox" id="' + id + '" checked />');
+                var label = $('<label for="' + id + '">' + label_text + '</label>');
+    
+                paragraph.append(checkbox);
+                paragraph.append(label);
+    
+                checkbox.change(function() {
+                    var id = this.id;
+    
+                    Plotly.restyle(plotDiv, 'visible', this.checked, traceIndicesByName[id]);
+                });
+    
+                checkboxForm.append(paragraph);
+            });
+        }
+    }
 
-    $('#wheat_pp_checkbox').change(function() {
-        Plotly.restyle(plot_elem, 'visible', this.checked, trace_indices_by_name['wheat_pp']);
-    });
-    $('#coal_pp_checkbox').change(function() {
-        Plotly.restyle(plot_elem, 'visible', this.checked, trace_indices_by_name['coal_pp']);
-    });
-    $('#diamond_pp_checkbox').change(function() {
-        Plotly.restyle(plot_elem, 'visible', this.checked, trace_indices_by_name['diamond_pp']);
-    });
-    $('#exp_market_checkbox').change(function() {
-        Plotly.restyle(plot_elem, 'visible', this.checked, trace_indices_by_name['exp_market']);
+    var expTable = [ // Min, max, sell, buy
+        [-Infinity, -1000, 1, 2],
+        [-999, -500, 2, 4],
+        [-499, -250, 4, 6],
+        [-249, -100, 6, 8],
+        [-99, -50, 10, 12],
+        [-49, 0, 12, 14],
+
+        [1, 49, 14, 16],
+        [50, 99, 16, 18],
+        [100, 249, 20, 24],
+        [250, 499, 24, 30],
+        [500, 999, 32, 38],
+        [1000, 1999, 40, 48],
+        [2000, 4999, 48, 64],
+        [5000, 9999, 56, 80],
+        [10000, Infinity, 64, 96]
+    ];
+
+    $.get('get-data', null, function(data) {
+
+        createPlot('powerplants', ['wheat_pp', 'coal_pp', 'diamond_pp'], data, 'Электростанции');
+
+        // Process exp market data
+        var expMarketData = data.exp_market;
+        var dates = expMarketData.dates;
+        var sellData = [];
+        var buyData = [];
+
+        $.each(expMarketData.values, function(val) {
+            for(var i = 0; i < expTable.length; i++) {
+                var expEntry = expTable[i];
+
+                if(val >= expEntry[0] && val <= expEntry[1]) {
+                    sellData.push(expEntry[2]);
+                    buyData.push(expEntry[3]);
+                }
+            }
+        });
+
+        createPlot('exp_market', ['buy', 'sell'], {
+            sell: {
+                dates: dates,
+                values: sellData
+            },
+            buy: {
+                dates: dates,
+                values: buyData
+            }
+        }, 'Опыт');
+
     });
 });
